732da5ffd5609cb3f97b61a7269e56d0f5667b86
joon <joon@sakeinu.ai>
2024-09-12 17:53:30 +0900

    feat: add txhash to the sent airdrop info

--- src/commands/sakeinu/airdrop.ts
+++ src/commands/sakeinu/airdrop.ts
@@ -1,17 +1,14 @@
-import { readFileSync } from 'fs'
-import { printTx } from '../../common/util'
+import { readFileSync, writeFileSync } from 'fs'
 import { SakeInu } from '../../contracts/sakeinu'
 import { Command } from '../interface'
 import { parseUnits } from 'ethers'
 
 interface airdrop {
   address: string
-  amount: string | number
+  amount: string
+  hash?: string
 }
 
-interface seiAddrReq {
-  sei_address: string
-}
 interface seiAddrRes {
   evm_address: string
   associated: boolean
@@ -30,34 +27,93 @@ export class SakeInuAirdrop implements Command {
       return false
     }
 
-    // add current path with filename
-    const data = readFileSync(`${process.cwd()}/${args[0]}`, 'utf-8')
+    const filePath = `${process.cwd()}/${args[0]}`
+    const data = readFileSync(filePath, 'utf-8')
     const airdrops: airdrop[] = JSON.parse(data)
+    const evmAddresses: string[] = new Array(airdrops.length)
     const restUrl = `https://rest.sei-apis.com/sei-protocol/seichain/evm/evm_address`
-    for (const target of airdrops) {
-      const req: seiAddrReq = { sei_address: target.address }
-      const params = new URLSearchParams(req as any)
 
-      const result = await fetch(`${restUrl}?${params.toString()}`)
-      if (!result.ok) {
+    // prepare evm addresses
+    console.log('Prepare evm address...')
+    for (let i = 0; i < airdrops.length; i++) {
+      const target = airdrops[i]
+      if (target.hash) {
+        continue
+      }
+
+      const params = new URLSearchParams({ sei_address: target.address })
+      let res: seiAddrRes
+      try {
+        const result = await fetch(`${restUrl}?${params.toString()}`)
+        if (!result.ok) {
+          console.error(`Error fetching address ${target.address}`)
+          // sleep
+          await new Promise((resolve) => setTimeout(resolve, 1000))
+          i--
+          continue
+        }
+        res = await result.json()
+        if (res.associated === false) {
+          console.warn(`${i + 1} Address ${target.address} is not associated`)
+          continue
+        }
+      } catch (err) {
         console.error(`Error fetching address ${target.address}`)
+        i--
+      }
+      evmAddresses[i] = res.evm_address
+    }
+
+    if (evmAddresses.length !== airdrops.length) {
+      console.error('Error fetching addresses')
+      return false
+    }
+
+    // start airdrop
+    console.log('Sending SAKE...')
+    for (let i = 0; i < airdrops.length; i++) {
+      const target = airdrops[i]
+      const evmAddress = evmAddresses[i]
+      if (target.hash) {
+        console.log(`${i + 1} Skipping ${target.address}`)
         continue
       }
-      const res: seiAddrRes = await result.json()
-      if (res.associated === false) {
-        console.error(`Address ${target.address} is not associated with SEI`)
+      if (!evmAddress) {
+        console.log(
+          `${i + 1} ${target.address} is not associated with an EVM address, skipping`,
+        )
         continue
       }
 
-      console.log(
-        `sending SAKE to ${res.evm_address}, amount: ${target.amount}`,
-      )
-      const tx = await this.si.transfer(
-        target.address,
-        parseUnits(target.amount.toString(), 18),
-      )
-      printTx(tx)
+      try {
+        const tx = await this.si.transfer(
+          target.address,
+          parseUnits(target.amount.toString(), 18),
+        )
+
+        // Wait for the transaction to be mined
+        const receipt = await tx.wait()
+        if (receipt.status !== 1) {
+          console.error(`Transaction failed: ${receipt.hash}`)
+          i--
+          await new Promise((resolve) => setTimeout(resolve, 1000))
+          continue
+        }
+
+        // success
+        target.hash = receipt.hash
+        console.log(
+          `${i + 1} Sent SAKE to ${target.address}(${evmAddress}), amount: ${target.amount}, tx: ${target.hash}`,
+        )
+      } catch (err) {
+        console.error(`Error sending SAKE to ${target.address}(${evmAddress})`)
+        break
+      }
     }
+
+    const updatedData = JSON.stringify(airdrops, null, 2)
+    writeFileSync(filePath, updatedData)
+
     return true
   }
 }
--- src/commands/sakeinu/checkAddress.ts
+++ src/commands/sakeinu/checkAddress.ts
@@ -31,22 +31,24 @@ export class SakeInuCheckAddress implements Command {
     const data = readFileSync(`${process.cwd()}/${args[0]}`, 'utf-8')
     const airdrops: airdrop[] = JSON.parse(data)
     const restUrl = `https://rest.sei-apis.com/sei-protocol/seichain/evm/evm_address`
-    for (const target of airdrops) {
+    for (let i = 0; i < airdrops.length; i++) {
+      const target = airdrops[i]
       const req: seiAddrReq = { sei_address: target.address }
       const params = new URLSearchParams(req as any)
 
       const result = await fetch(`${restUrl}?${params.toString()}`)
       if (!result.ok) {
-        console.error(`Error fetching address ${target.address}`)
+        console.error(`Error fetching address ${target.address}, index: ${i}`)
+        i--
         continue
       }
       const res: seiAddrRes = await result.json()
       if (res.associated === false) {
-        console.error(`Address ${target.address} is not associated with SEI`)
+        console.error(`${i + 1} ${target.address} is not associated`)
         continue
       }
 
-      console.log(`sending SAKE from ${target.address} to ${res.evm_address}`)
+      console.log(`${i + 1} ${target.address} is associated ${res.evm_address}`)
     }
     return true
   }
